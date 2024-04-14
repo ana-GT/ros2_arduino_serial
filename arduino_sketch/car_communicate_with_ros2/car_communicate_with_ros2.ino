329a5eab410e5c1de551ecccd73de32a4695252a
Ana C. Huaman Quispe <ana@traclabs.com>
2024-04-14 16:17:57 -0500

    Added final version of car

--- arduino_sketch/car_communicate_with_ros2/car_communicate_with_ros2.ino
+++ arduino_sketch/car_communicate_with_ros2/car_communicate_with_ros2.ino
@@ -2,7 +2,7 @@
 
 const byte numChars = 32;
 char receivedChars[numChars];
-char temp_chars_[numChars];        // temporary array for use when parsing
+char temp_chars_[numChars];
 
 // variables to hold the parsed data
 char label_lin_[numChars] = {0};
@@ -11,12 +11,22 @@ float twist_lin_ = 0.0;
 float twist_ang_ = 0.0;
 boolean newData = false;
 
+// Motors
+const int A_1B = 5;
+const int A_1A = 6;
+const int B_1B = 9;
+const int B_1A = 10;
+
 String debug_string_;
 
 
 void setup() {
     Serial.begin(9600);
-    //Serial.println("Enter data in this style <HelloWorld, 12, 24.7>  ");
+
+    pinMode(A_1B, OUTPUT);
+    pinMode(A_1A, OUTPUT);
+    pinMode(B_1B, OUTPUT);
+    pinMode(B_1A, OUTPUT);
 }
 
 void loop() {
@@ -26,7 +36,8 @@ void loop() {
             // this temporary copy is necessary to protect the original data
             //   because strtok() used in parseData() replaces the commas with \0
         parseData();
-        showParsedData();
+        motionCommand();
+        debugShowData();
         newData = false;
     }
 }
@@ -83,16 +94,74 @@ void parseData() {      // split the data into its parts
     strtokIndx = strtok(NULL, ",");
     twist_ang_ = atof(strtokIndx);     // convert this part to a float
 
-
     //strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
     //integerFromPC = atoi(strtokIndx);     // convert this part to an integer
     
-    debug_string_ = String(label_lin_) + String(": ") + String(twist_lin_, 3) + String(" --- ") + String(label_ang_) + String(": ") + String(twist_ang_, 3) ; 
-
 }
 
 //============
 
-void showParsedData() {
+void debugShowData() {
     Serial.println(debug_string_);
+}
+
+void motionCommand() {
+
+  int vel_left, vel_right;
+  calculate_speeds(twist_lin_, twist_ang_, vel_left, vel_right);
+   
+  motionAction(vel_left, vel_right);
+
+  debug_string_ = String(label_lin_) + String(": ") + String(twist_lin_, 3) + String(" --- ") + String(label_ang_) + String(": ") + String(twist_ang_, 3) ; 
+  debug_string_ = debug_string_ + " ** " + " vel_left: " + String(vel_left) + " vel_right: " + String(vel_right);
+}
+
+void calculate_speeds(float vlin, float vang, int &vleft, int &vright)
+{
+  float lwr = 0.05*1; // wheel radius
+  float ws = 0.20; // wheel separation
+
+  float vleft_float = ( twist_lin_ - twist_ang_*ws/2.0 )/ lwr;
+  float vright_float = ( twist_lin_ + twist_ang_*ws/2.0 )/ lwr;
+
+  // Calculate scaled values w.r.t. max value
+  // consider: max value for vleft/vright=255 when vlin=1
+  float vmax = 26.5;
+  vleft = (int)(vleft_float/vmax * 255);
+  vright = (int)(vright_float/vmax * 255);
+
+  applyLimits(vleft, vright);  
+}
+
+void motionAction(int vleft, int vright)
+{
+  if (vleft >= 0)
+  {
+   analogWrite(A_1B, vleft);
+   analogWrite(A_1A, 0);
+  } else {
+   analogWrite(A_1B, 0);
+   analogWrite(A_1A, abs(vleft));    
+  }
+
+  if (vright >= 0)
+  {
+   analogWrite(B_1B, 0);
+   analogWrite(B_1A, vright);
+  } else {
+    analogWrite(B_1B, abs(vright));
+    analogWrite(B_1A, 0);
+  }
+}
+
+void applyLimits(int &vleft, int &vright)
+{
+   if (vleft < -255)
+     vleft = -255;
+   if (vleft > 255)
+     vleft = 255;
+   if (vright < -255)
+     vright = -255;
+   if (vright > 255)
+     vright = 255;
 }
--- ros2_arduino_serial/car_comm.py
+++ ros2_arduino_serial/car_comm.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+import math
 
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
@@ -15,14 +16,26 @@ class CarCommSerial(Node):
     def __init__(self, serial_port, baud_rate):
     
         super().__init__('car_comm_serial')
-        self.subscription = self.create_subscription(
+        self.sub_twist_ = self.create_subscription(
             Twist,
             'cmd_vel',
             self.twist_callback,
             10)
-        self.subscription  # prevent unused variable warning
-        self.timer = self.create_timer(1, self.timer_callback)
+        self.sub_twist_  # prevent unused variable warning
+        
+        self.pub_odom_ = self.create_publisher(Odometry, '/odom', 10)
+        self.odom_ = Odometry();
+        self.odom_.header.frame_id = "world"
+        self.odom_.child_frame_id = "car"
+        self.x_ = 0;
+        self.y_ = 0;
+        self.theta_ = 0;
+        
+        self.timer_dt_ = 1.0;
+        self.timer = self.create_timer(self.timer_dt_, self.timer_callback)
         self.twist_ = Twist()
+        self.twist_last_time_ = self.get_clock().now()
+        self.max_dt_ = 2.0;
 
         # Serial
         self.serial_ = serial.Serial(serial_port, baud_rate, timeout=1)
@@ -31,22 +44,44 @@ class CarCommSerial(Node):
 
     def twist_callback(self, msg):
         self.twist_ = msg
+        self.twist_last_time_ = self.get_clock().now()
+        #self.get_logger().info("Twist callback, lin: {} and ang: {}".format(self.twist_.linear.x, self.twist_.angular.z))
 
     def timer_callback(self):
-        self.get_logger().info("Timer call")
+        #self.get_logger().info("Timer call")
+       
+        # If twist message is old, do not send anything
+        if (self.get_clock().now().nanoseconds - self.twist_last_time_.nanoseconds)*1e-9 > self.max_dt_ :
+          return; 
        
         # Send twist
-        self.get_logger().info("Received twist: Lin: {} ang: {}".format(self.twist_.linear.x, self.twist_.angular.z))
+        #self.get_logger().info("From twist msg: Lin: {} ang: {}".format(self.twist_.linear.x, self.twist_.angular.z))
         
         command = "<" + "vel_lin" + "," + str(self.twist_.linear.x) + ", " + "vel_ang" + "," + str(self.twist_.angular.z) +  ">\n"
-        self.get_logger().info("** Command sent: {}".format(command))
+        #self.get_logger().info("\t ** Command sent: {}".format(command))
         
         self.serial_.write(command.encode('utf-8'))
         self.rate_.sleep()
         
+        # Update odometry
+        vlin = self.twist_.linear.x
+        vang = self.twist_.angular.z
+        
+        self.theta_ += vang*self.timer_dt_
+        self.x_ += vlin*math.cos(self.theta_)*self.timer_dt_
+        self.y_ += vlin*math.sin(self.theta_)*self.timer_dt_
+        
+        # Update odometry message
+        self.odom_.pose.pose.position.x = self.x_ 
+        self.odom_.pose.pose.position.y = self.y_
+        self.odom_.pose.pose.orientation.z = math.sin(self.theta_/2.0);
+        self.odom_.pose.pose.orientation.w = math.cos(self.theta_/2.0);
+        self.odom_.header.stamp = self.get_clock().now().to_msg()
+        self.pub_odom_.publish(self.odom_)
+        
         # Check if data back is being received from car
         line = self.serial_.readline().decode('utf-8').rstrip()
-        self.get_logger().info("Received from Arduino: {}".format(line))
+        self.get_logger().info("From Arduino: {}".format(line))
         
 
 ###################################################
